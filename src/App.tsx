f195c41103d3c9d1b4b6ba0cb383d04c16927879
theluanz <luanzuffo.lz@gmail.com>
2022-09-11 21:44:01 -0300

    [FIX] Remove botoes

--- src/App.tsx
+++ src/App.tsx
@@ -74,14 +74,7 @@ function App() {
           <canvas
             id="canvas"
             className="w-full border border-gray-400 rounded-lg shadow-lg"></canvas>
-          <div className="w-full flex justify-between">
-            <button className="px-4 py-2 mt-2 bg-blue-400 shadow  text-blue-900 rounded-lg hover:bg-blue-500hover:text-white hover:shadow-lg transition-colors">
-              Desenhar
-            </button>
-            <button className="px-4 py-2 mt-2 bg-blue-400 shadow  text-blue-900 rounded-lg hover:bg-blue-500hover:text-white hover:shadow-lg transition-colors">
-              Limpar
-            </button>
-          </div>
+          
         </div>
         <div className="col-span-1 h-full">
           <div className="flex gap-2">
